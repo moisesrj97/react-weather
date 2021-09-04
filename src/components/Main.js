d2dedc58824446f1f3c3c803868d9db165ebc3eb
moisesrj97 <moisesrj97@gmail.com>
2021-09-04 14:16:46 +0200

    basic current display and half-implementation of google places api in searchbar

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,9 @@ import './Main.scss';
 const Main = (props) => {
   const { weatherData } = props;
   const { locationInfo, futureWeather } = weatherData;
+
+  console.log(locationInfo);
+
   if (Object.keys(weatherData).length === 0) {
     return (
       <div className='Main'>
@@ -11,27 +14,60 @@ const Main = (props) => {
       </div>
     );
   } else {
+    const sunrise = new Date(locationInfo.sys.sunrise * 1000);
+    const sunset = new Date(locationInfo.sys.sunset * 1000);
+
+    const sunriseFix =
+      sunrise.getUTCHours() +
+      locationInfo.timezone / 3600 +
+      ':' +
+      sunrise.getUTCMinutes();
+    const sunsetFix =
+      sunset.getUTCHours() +
+      locationInfo.timezone / 3600 +
+      ':' +
+      sunset.getUTCMinutes();
+
     return (
       <div className='Main'>
-        <div className='basic-info'>
-          <div>
-            <h1>
+        <div className='main-content'>
+          <div className='basic-info'>
+            <h1 className='city-name'>
               {locationInfo.name}, {locationInfo.sys.country}
             </h1>
-            <p>{locationInfo.main.temp}ºC</p>
-            <div>
+            <div className='city-temp'>
+              <p className='current-temp'>{locationInfo.main.temp}ºC</p>
+              <p className='min-max'>
+                Min/Max: {locationInfo.main.temp_min}ºC/
+                {locationInfo.main.temp_max}ºC
+              </p>
+              <p className='current-feeling'>
+                Feeling: {locationInfo.main.feels_like}ºC
+              </p>
+            </div>
+            <div className='icon-info'>
               <img
-                src={`https://openweathermap.org/img/wn/${locationInfo.weather[0].icon}.png`}
-                alt='Weather icon'
+                className='climate-icon'
+                src={`https://openweathermap.org/img/wn/${locationInfo.weather[0].icon}@2x.png`}
+                alt={locationInfo.weather[0].main}
+                title={locationInfo.weather[0].main}
               />
+              <i className='fas fa-tint'></i>
+              <span className='humidity-data' title='Humidity'>
+                {locationInfo.main.humidity}%
+              </span>
+            </div>
+            <div className='time-info-container'>
+              <div className='time-info-item'>
+                <i className='far fa-sun' title='Sunrise'></i>
+                <span className='humidity-data'>{sunriseFix}</span>
+              </div>
+              <div className='time-info-item'>
+                <i className='far fa-moon' title='Sunset'></i>
+                <span className='humidity-data'>{sunsetFix}</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='advanced-info'>
-          <p>Humidity: {locationInfo.main.humidity}</p>
-          <p>Max temp: {locationInfo.main.temp_max}</p>
-          <p>Min temp: {locationInfo.main.temp_min}</p>
-          <p>Feels Like: {locationInfo.main.feels_like}</p>
         </div>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,86 @@
 import React, { useState } from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+
 import './Navbar.scss';
 
 const Navbar = (props) => {
-  const [location, setLocation] = useState('');
+  const [address, setAddress] = useState('');
 
-  const handleInputChange = (evt) => {
-    setLocation(evt.target.value);
+  const handleChange = (address) => {
+    console.log(address);
+    setAddress(address);
   };
+
+  const handleSelect = (address) => {
+    geocodeByAddress(address)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => console.log('Success', latLng))
+      .catch((error) => console.error('Error', error));
+  };
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    props.handleSearch(location);
-    setLocation('');
+    props.handleSearch(address);
+    setAddress('');
   };
 
   return (
     <div className='Navbar'>
       <form className='search-form' onSubmit={handleSubmit}>
-        <i className='fas fa-search'></i>
+        <PlacesAutocomplete
+          value={address}
+          onChange={handleChange}
+          onSelect={handleSelect}
+        >
+          {({
+            getInputProps,
+            suggestions,
+            getSuggestionItemProps,
+            loading,
+          }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: 'Search Places ...',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className='autocomplete-dropdown-container'>
+                {loading && <div>Loading...</div>}
+                {suggestions.map((suggestion) => {
+                  const className = suggestion.active
+                    ? 'suggestion-item--active'
+                    : 'suggestion-item';
+                  // inline style for demonstration purpose
+                  const style = suggestion.active
+                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+        {/* <i className='fas fa-search'></i>
         <input
           className='search-bar'
           type='text'
           placeholder='Enter a location...'
           onChange={handleInputChange}
           value={location}
-        ></input>
+        ></input> */}
       </form>
     </div>
   );
